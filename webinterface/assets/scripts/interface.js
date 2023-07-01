302d668878e5fae9c28091381b1842aefc31194d
Astrix Game <mail.astrixgame@gmail.com>
2023-07-01 13:14:52 +0200

    Updated Files

--- webinterface/assets/scripts/interface.js
+++ webinterface/assets/scripts/interface.js
@@ -170,8 +170,8 @@ connection.onopen = function() {
                                 opennedPanelVal = '<div class="openned-value-item"><button class="irrigation" onclick="lxControl(this, \'Irrigation\')" id="'+lxData.controls[uuid].states.rainActive+'-button-open"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg></button></div>';
                             break;
                             case "Intercom":
-                                value = '<button class="intercom" onclick="lxControl(this, \'Intercom\')" id="'+uuid+'-button"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zM559.1 99.8c10.4 5.6 16.9 16.4 16.9 28.2V384c0 11.8-6.5 22.6-16.9 28.2s-23 5-32.9-1.6l-96-64L416 337.1V320 192 174.9l14.2-9.5 96-64c9.8-6.5 22.4-7.2 32.9-1.6z"/></svg></button>';
-                                opennedPanelVal = '';// ###
+                                //value = '<button class="intercom" onclick="lxControl(this, \'Intercom\')" id="'+uuid+'-button"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zM559.1 99.8c10.4 5.6 16.9 16.4 16.9 28.2V384c0 11.8-6.5 22.6-16.9 28.2s-23 5-32.9-1.6l-96-64L416 337.1V320 192 174.9l14.2-9.5 96-64c9.8-6.5 22.4-7.2 32.9-1.6z"/></svg></button>';
+                                //opennedPanelVal = '';// ###
                             break;
                             case "SmokeAlarm":
                                 value = '<txt class="value" id="'+lxData.controls[uuid].states.areAlarmSignalsOff+'-value">N/A</txt>';
