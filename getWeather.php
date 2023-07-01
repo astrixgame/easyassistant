a9fe14c0f1eb8964a0511fb8eff6c789f7d1d898
Astrix Game <mail.astrixgame@gmail.com>
2023-07-01 15:08:07 +0200

    Organizated Files

--- getWeather.php
+++
@@ -1,3 +0,0 @@
-<?php
-    file_put_contents("weather.json",file_get_contents("http://api.openweathermap.org/data/2.5/forecast?lat=17.0833247&lon=49.4722386&appid=3c0068820a0afab5fe16aa372e03278f"));
-?>
