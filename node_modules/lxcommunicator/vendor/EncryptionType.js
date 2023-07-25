bed33c245cf8ae40c75bb535ba9970ad444fa3d4
astrixgame <78021940+astrixgame@users.noreply.github.com>
2023-07-25 14:53:45 +0200

    Remove libraries from git

--- node_modules/lxcommunicator/vendor/EncryptionType.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    module.exports = {
-        NONE: 0,
-        REQUEST: 1,                 // the request is encrypted
-        REQUEST_RESPONSE_VAL: 2     // the request + the response value (only value!) is encrypted
-    };
-}).call(this);
