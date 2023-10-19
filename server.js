a8fd6913fc02d0055731a097edd67422615a517e
AstrixGame <78021940+astrixgame@users.noreply.github.com>
2023-10-19 12:21:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -136,13 +136,8 @@ function createNetServer() {
         var contentType = mimeTypes[extname] || 'application/octet-stream';
         fs.readFile(filePath, (error, content) => {
             if(error) {
-                if(error.code === 'ENOENT') {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
-                    res.end('<h1>404 Not Found</h1>');
-                } else {
-                    res.writeHead(500);
-                    res.end(`Server Error: ${error.code}`);
-                }
+                res.writeHead(500);
+                res.end(`Server Error: ${error.code}`);
             } else {
                 res.writeHead(200, { 'Content-Type': contentType });
                 res.end(content, 'utf-8');
